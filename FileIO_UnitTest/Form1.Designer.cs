a6721ff104456b14f3166d284914abe53ca93425
NessRK13 <ness6514@student.cptc.edu>
2019-03-05 14:44:57 -0800

    Added a button to the main form for the button click event.

--- FileIO_UnitTest/Form1.Designer.cs
+++ FileIO_UnitTest/Form1.Designer.cs
@@ -26,6 +26,7 @@
             this.tbxUserInput = new System.Windows.Forms.TextBox();
             this.btnSaveData = new System.Windows.Forms.Button();
             this.btnUploadData = new System.Windows.Forms.Button();
+            this.btnEncryptText = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbxUserInput
@@ -40,7 +41,7 @@
             // 
             this.btnSaveData.Location = new System.Drawing.Point(12, 268);
             this.btnSaveData.Name = "btnSaveData";
-            this.btnSaveData.Size = new System.Drawing.Size(150, 50);
+            this.btnSaveData.Size = new System.Drawing.Size(128, 50);
             this.btnSaveData.TabIndex = 1;
             this.btnSaveData.Text = "Save Text to txt File";
             this.btnSaveData.UseVisualStyleBackColor = true;
@@ -48,18 +49,28 @@
             // 
             // btnUploadData
             // 
-            this.btnUploadData.Location = new System.Drawing.Point(168, 268);
+            this.btnUploadData.Location = new System.Drawing.Point(146, 268);
             this.btnUploadData.Name = "btnUploadData";
-            this.btnUploadData.Size = new System.Drawing.Size(150, 50);
+            this.btnUploadData.Size = new System.Drawing.Size(128, 50);
             this.btnUploadData.TabIndex = 2;
             this.btnUploadData.Text = "Upload txt File";
             this.btnUploadData.UseVisualStyleBackColor = true;
+            // 
+            // btnEncryptText
+            // 
+            this.btnEncryptText.Location = new System.Drawing.Point(280, 269);
+            this.btnEncryptText.Name = "btnEncryptText";
+            this.btnEncryptText.Size = new System.Drawing.Size(128, 49);
+            this.btnEncryptText.TabIndex = 3;
+            this.btnEncryptText.Text = "Encrypt Text";
+            this.btnEncryptText.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 329);
+            this.ClientSize = new System.Drawing.Size(523, 329);
+            this.Controls.Add(this.btnEncryptText);
             this.Controls.Add(this.btnUploadData);
             this.Controls.Add(this.btnSaveData);
             this.Controls.Add(this.tbxUserInput);
@@ -75,6 +86,7 @@
         private System.Windows.Forms.TextBox tbxUserInput;
         private System.Windows.Forms.Button btnSaveData;
         private System.Windows.Forms.Button btnUploadData;
+        private System.Windows.Forms.Button btnEncryptText;
     }
 }
 
