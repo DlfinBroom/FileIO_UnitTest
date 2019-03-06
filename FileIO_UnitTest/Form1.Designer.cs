e4579caefa5644c3f0ecb1b90647779112c3de51
NessRK13 <ness6514@student.cptc.edu>
2019-03-05 18:32:37 -0800

    Created the Decrypt button on the main form and created the Decrypt event

--- FileIO_UnitTest/Form1.Designer.cs
+++ FileIO_UnitTest/Form1.Designer.cs
@@ -27,11 +27,12 @@
             this.btnSaveData = new System.Windows.Forms.Button();
             this.btnUploadData = new System.Windows.Forms.Button();
             this.btnEncryptText = new System.Windows.Forms.Button();
+            this.btnDecryptText = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbxUserInput
             // 
-            this.tbxUserInput.Location = new System.Drawing.Point(12, 12);
+            this.tbxUserInput.Location = new System.Drawing.Point(11, 12);
             this.tbxUserInput.Multiline = true;
             this.tbxUserInput.Name = "tbxUserInput";
             this.tbxUserInput.Size = new System.Drawing.Size(500, 250);
@@ -39,9 +40,9 @@
             // 
             // btnSaveData
             // 
-            this.btnSaveData.Location = new System.Drawing.Point(12, 268);
+            this.btnSaveData.Location = new System.Drawing.Point(12, 267);
             this.btnSaveData.Name = "btnSaveData";
-            this.btnSaveData.Size = new System.Drawing.Size(128, 50);
+            this.btnSaveData.Size = new System.Drawing.Size(121, 50);
             this.btnSaveData.TabIndex = 1;
             this.btnSaveData.Text = "Save Text to txt File";
             this.btnSaveData.UseVisualStyleBackColor = true;
@@ -49,9 +50,9 @@
             // 
             // btnUploadData
             // 
-            this.btnUploadData.Location = new System.Drawing.Point(146, 268);
+            this.btnUploadData.Location = new System.Drawing.Point(136, 267);
             this.btnUploadData.Name = "btnUploadData";
-            this.btnUploadData.Size = new System.Drawing.Size(128, 50);
+            this.btnUploadData.Size = new System.Drawing.Size(121, 50);
             this.btnUploadData.TabIndex = 2;
             this.btnUploadData.Text = "Upload txt File";
             this.btnUploadData.UseVisualStyleBackColor = true;
@@ -59,19 +60,30 @@
             // 
             // btnEncryptText
             // 
-            this.btnEncryptText.Location = new System.Drawing.Point(280, 269);
+            this.btnEncryptText.Location = new System.Drawing.Point(263, 267);
             this.btnEncryptText.Name = "btnEncryptText";
-            this.btnEncryptText.Size = new System.Drawing.Size(128, 49);
+            this.btnEncryptText.Size = new System.Drawing.Size(121, 50);
             this.btnEncryptText.TabIndex = 3;
             this.btnEncryptText.Text = "Encrypt Text";
             this.btnEncryptText.UseVisualStyleBackColor = true;
             this.btnEncryptText.Click += new System.EventHandler(this.btnEncryptText_Click);
+            // 
+            // btnDecryptText
+            // 
+            this.btnDecryptText.Location = new System.Drawing.Point(390, 267);
+            this.btnDecryptText.Name = "btnDecryptText";
+            this.btnDecryptText.Size = new System.Drawing.Size(121, 50);
+            this.btnDecryptText.TabIndex = 4;
+            this.btnDecryptText.Text = "Decrypt Text";
+            this.btnDecryptText.UseVisualStyleBackColor = true;
+            this.btnDecryptText.Click += new System.EventHandler(this.btnDecryptText_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(523, 329);
+            this.Controls.Add(this.btnDecryptText);
             this.Controls.Add(this.btnEncryptText);
             this.Controls.Add(this.btnUploadData);
             this.Controls.Add(this.btnSaveData);
@@ -89,6 +101,7 @@
         private System.Windows.Forms.Button btnSaveData;
         private System.Windows.Forms.Button btnUploadData;
         private System.Windows.Forms.Button btnEncryptText;
+        private System.Windows.Forms.Button btnDecryptText;
     }
 }
 
